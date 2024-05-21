40a7cc302a8e74577b2338207fde7a78549351c0
Miguel Bohrz <miguelrbohrz@gmail.com>
2024-05-20 21:25:53 -0300

    refatorações gerais

--- src/main/kotlin/com/fiap/fastfood/core/domain/Order.kt
+++ src/main/kotlin/com/fiap/fastfood/core/domain/Order.kt
@@ -2,33 +2,23 @@ package com.fiap.fastfood.core.domain
 
 import com.fiap.fastfood.core.entity.OrderEntity
 import com.fiap.fastfood.core.valueObject.Status
-import jakarta.validation.constraints.NotEmpty
-import java.sql.Timestamp
+import java.time.LocalDateTime
 
 class Order(
-    val id: String? = null,
-    val totalAmount: Double,
-    val customerId: String,
-    val isPayed: Boolean,
-    val status: Status,
-    @field:NotEmpty val products: List<Product>,
-    val createdAt: Timestamp? = null,
-    val updatedAt: Timestamp? = null
+    private val id: String,
+    private val totalAmount: Double,
+    private val customer: Customer,
+    private val isPayed: Boolean,
+    private val status: Status,
+    private val products: List<Product>,
+    private val createdAt: LocalDateTime,
+    private val updatedAt: LocalDateTime
 ) {
     fun hasCombo(): Boolean {
         return this.products.isNotEmpty()
     }
 
     fun toEntity(): OrderEntity {
-        return OrderEntity(
-            id,
-            totalAmount,
-            Customer(customerId, null.toString(), null.toString(), null.toString(), null, null, null).toEntity(),
-            isPayed,
-            status,
-            products.map { it.toEntity() },
-            createdAt,
-            updatedAt
-        )
+        return OrderEntity(id, totalAmount, customer.toEntity(), isPayed, status, products.map { it.toEntity() }, createdAt, updatedAt)
     }
 }
