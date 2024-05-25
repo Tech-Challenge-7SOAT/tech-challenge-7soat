611fe94aa9c995c48f699198d70631acaa0f93e0
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2024-05-25 13:44:38 -0300

    Adicionado swagger annotation para documentação.

--- src/main/kotlin/com/fiap/fastfood/adapter/driver/controller/CustomerController.kt
+++ src/main/kotlin/com/fiap/fastfood/adapter/driver/controller/CustomerController.kt
@@ -4,6 +4,8 @@ import com.fiap.fastfood.core.application.port.CustomerInterface
 import com.fiap.fastfood.core.application.port.repository.CustomerRepository
 import com.fiap.fastfood.core.domain.Customer
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -21,6 +23,12 @@ class CustomerController(
     @PostMapping("/customer")
     @Operation(summary = "Create a costumer")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "Customer created successfully"),
+        ApiResponse(responseCode = "400", description = "When the CPF already exists in database"),
+        ApiResponse(responseCode = "404", description = "When can't find a customer using the CPF number"),
+        ApiResponse(responseCode = "500", description = "When it isn't possible to create the customer")
+    ])
     fun saveCustomer(@RequestBody @Valid customer: Customer): Customer {
         if (customerRepository.existsByCpf(customer.cpf)) {
             throw ResponseStatusException(
@@ -32,6 +40,11 @@ class CustomerController(
 
     @GetMapping("/customer/{cpf}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Returns the data of specific customer."),
+        ApiResponse(responseCode = "404", description = "When don't find a customer using the CPF number specific."),
+        ApiResponse(responseCode = "500", description = "When it is not possible to find the customer.")
+    ])
     fun fetchCustomerByCpf(@PathVariable("cpf") cpf: String ) : Customer? {
         if (!customerRepository.existsByCpf(cpf)) {
             throw ResponseStatusException(
@@ -41,8 +54,11 @@ class CustomerController(
         return customerInterface.findByCpf(cpf)
     }
 
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Returns a list with all customers of database."),
+        ApiResponse(responseCode = "500", description = "When it is not possible to find a list of customers.")
+    ])
     fun fetchAllCustomers(): Collection<Customer> = customerInterface.fetchAllCustomers()
 }
