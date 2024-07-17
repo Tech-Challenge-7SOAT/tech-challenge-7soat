19172ee6ea1b31e56fe573acedd3cf5fc4c82eed
Rafael Acioly <aciolyr@gmail.com>
2024-07-16 21:35:22 -0300

    Add endpoint to retrieve status

--- src/main/kotlin/com/fiap/fastfood/core/valueObject/Status.kt
+++ src/main/kotlin/com/fiap/fastfood/core/valueObject/Status.kt
@@ -3,6 +3,8 @@ package com.fiap.fastfood.core.valueObject
 enum class Status {
     PRONTO,
     EM_PREPARACAO,
+    PAGAMENTO_APROVADO,
+    PAGAMENTO_RECUSADO,
     RECEBIDO,
     FINALIZADO,
     PENDENTE
