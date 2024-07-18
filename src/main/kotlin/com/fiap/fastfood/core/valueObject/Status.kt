93514f8c744e948b58bb4ec9ab44d1c45b087b29
Rafael Acioly <aciolyr@gmail.com>
2024-07-17 21:26:41 -0300

    Reorder status

--- src/main/kotlin/com/fiap/fastfood/core/valueObject/Status.kt
+++ src/main/kotlin/com/fiap/fastfood/core/valueObject/Status.kt
@@ -3,9 +3,9 @@ package com.fiap.fastfood.core.valueObject
 enum class Status {
     PRONTO,
     EM_PREPARACAO,
-    PAGAMENTO_APROVADO,
-    PAGAMENTO_RECUSADO,
     RECEBIDO,
     FINALIZADO,
-    PENDENTE
+    PENDENTE,
+    PAGAMENTO_APROVADO,
+    PAGAMENTO_RECUSADO,
 }
